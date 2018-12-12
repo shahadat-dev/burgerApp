29e24f92610f20d062074790dc0361cf2127b6fb
Shahadat Hossain <shahadat.cseru@gmail.com>
2018-12-13 01:34:51 +0600

    Folder structure re-designed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 
-import Aux from './hoc/Aux';
-import Layout from './components/Layout/Layout';
+import Aux from './hoc/Aux/Aux';
+import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
   render() {
     return (
-    	<Aux>
-    		<Layout>
-	      	<BurgerBuilder />
-	      </Layout>
-	    </Aux>
+      <Aux>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
+      </Aux>
     );
   }
 }
